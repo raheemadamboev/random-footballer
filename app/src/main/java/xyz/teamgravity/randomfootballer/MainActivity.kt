cca026e56e6fcdea765fa452b0c1a6065438943f
Raheem <raheemideadev@gmail.com>
2021-11-06 00:48:24 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/randomfootballer/MainActivity.kt
@@ -0,0 +1,24 @@
+package xyz.teamgravity.randomfootballer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import xyz.teamgravity.randomfootballer.ui.theme.RandomFootballerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RandomFootballerTheme {
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
